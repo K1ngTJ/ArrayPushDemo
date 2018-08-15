7826c354f3852c73181737f89fb470eb4fc2014c
K1ngTJ <42422031+K1ngTJ@users.noreply.github.com>
2018-08-15 12:16:04 -1000

    First Commint
    Take variables and store to a array

---
+++ demopush.js
@@ -0,0 +1,15 @@
+var lastnames = []
+var firstnames = []
+
+var firstname = prompt("what is your First Name?");
+
+var lastname = prompt("What is your Last Name?");
+
+window.alert (firstname + lastname);
+
+console.log(firstnames.push(firstname));
+console.log(lastnames.push(lastname));
+console.log(lastnames + firstnames);
+
+
+window.alert(firstnames+""+lastnames+""+firstname.length+""+lastnames.length);
